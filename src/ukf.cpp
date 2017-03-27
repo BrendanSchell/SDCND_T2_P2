b611052b341e1ca9dd037f5b09ecb6284fb899f8
Brendan <brendanschell1@gmail.com>
2017-03-27 15:49:44 -0300

    Updated ProcessMeasurement function with skeleton:
    * Calculates timestep delta
    * Performes initialization check
    * Performs prediction (to be implemented)
    * Performs update conditionally on measurement type (functions to be implemented)

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -10,7 +10,7 @@ UKF::UKF() {
 
   // if this is false, radar measurements will be ignored (except during init)
   use_radar_ = true;
-
+  is_initialized_ = false;
   // initial state vector
   x_ = VectorXd(5);
   x_ << 0, 0, 0, 0, 0;
@@ -51,7 +51,7 @@ UKF::UKF() {
   */
   Xsig_pred_ = VectorXd(5);
   Xsig_pred_ << 0, 0, 0, 0, 0;
-  time_us = 0.;
+  time_us_ = 0.;
   weights_ = VectorXd(11);
   weights_ << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
   NIS_radar_ = 0.;
@@ -76,67 +76,49 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (!is_initialized_){
+    time_us_ = meas_package.timestamp_;
+    
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR){
+      float x_cart = meas_package.raw_measurements_[0] * cos(meas_package.raw_measurements_[1]);
+      float y_cart = meas_package.raw_measurements_[0] * sin(meas_package.raw_measurements_[1]);
+      if (x_cart == 0 or y_cart == 0){
+        return;
+      }
+      x_ << x_cart, y_cart, 0, 0, 0;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::LASER){
+      x_ << meas_package.raw_measurements[0], meas_package.raw_measurements[1],0 ,0, 0;
+    }
+    is_initialized = true;
+    return;
+  }
 
+  //calculate time delta between current and previous timestamp
+  float dt = (meas_package.timestamp_ - time_us_)/1000000.0;
+
+  //Update state transition matrix
+
+
+  //Update process noise covariance matrix
+
+  //set previous timestamp to current timestamp
+  time_us_ = meas_package.timestamp_;
+
+  //perform prediction step
+  this.Prediction(dt
+
+  //Perform updates based on measurement type
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR){
+    this.UpdateRadar(meas_package);
+  } 
+  else{
+    this.UpdateLidar(meas_package);
+  }
+  cout << "x_ = " << x_ << endl;
+  cout << "P_ = " << P_ << endl;
 }
 
-/**
- * Predicts sigma points, the state, and the state covariance matrix.
- * @param {double} delta_t the change in time (in seconds) between the last
- * measurement and this one.
- */
-
-  //define spreading parameter
-  double lambda = 3 - n_aug;
-
-  //create example matrix with predicted sigma points
-  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
-  Xsig_pred <<
-         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
-           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
-          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
-         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
-          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
-
-  //create vector for weights
-  VectorXd weights = VectorXd(2*n_aug+1);
-  
-  //create vector for predicted state
-  VectorXd x = VectorXd(n_x);
-
-  //create covariance matrix for prediction
-  MatrixXd P = MatrixXd(n_x, n_x);
-
-
-/*******************************************************************************
- * Student part begin
- ******************************************************************************/
-
-  // set weights
-  double weight_0 = lambda/(lambda+n_aug);
-  weights(0) = weight_0;
-  for (int i=1; i<2*n_aug+1; i++) {  //2n+1 weights
-    double weight = 0.5/(n_aug+lambda);
-    weights(i) = weight;
-  }
-
-  //predicted state mean
-  x.fill(0.0);
-  for (int i = 0; i < 2 * n_aug + 1; i++) {  //iterate over sigma points
-    x = x+ weights(i) * Xsig_pred.col(i);
-  }
-
-  //predicted state covariance matrix
-  P.fill(0.0);
-  for (int i = 0; i < 2 * n_aug + 1; i++) {  //iterate over sigma points
-
-    // state difference
-    VectorXd x_diff = Xsig_pred.col(i) - x;
-    //angle normalization
-    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
-    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
-
-    P = P + weights(i) * x_diff * x_diff.transpose() ;
-  }
 void UKF::Prediction(double delta_t) {
   /**
   TODO:
