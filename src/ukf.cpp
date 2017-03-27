85d30a88e9fdc694b59a59ddc69c9791d9011df2
Brendan <brendanschell1@gmail.com>
2017-03-27 17:41:03 -0300

    Filled in UpdateRadar function and small fixes: * Fixed properties missing trailing _ * Filled in UpdateRadar function

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -50,18 +50,18 @@ UKF::UKF() {
   Hint: one or more values initialized above might be wildly off...
   */
   time_us_ = 0.;
-  weights_ = VectorXd(11);
-  weights_ << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
   NIS_radar_ = 0.;
   NIS_laser_ = 0.;
 //set state dimension
-  n_x = 5;
+  n_x_ = 5;
 
   //set augmented dimension
-  n_aug = 7;
+  n_aug_ = 7;
   //define spreading parameter
-  lambda = 3 - n_aug;
-  Xsig_pred_ = MatrixXd(n_x, 2 * n_aug + 1);
+  lambda_ = 3 - n_aug_;
+  weights_ = VectorXd(2 * n_aug_ + 1);
+  weights_ << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
   
 }
 
@@ -122,13 +122,13 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 }
 void UKF::GenerateSigmaPoints(double delta_t) {
   //create augmented mean vector
-  VectorXd x_aug = VectorXd(n_aug);
+  VectorXd x_aug = VectorXd(n_aug_);
   
   //create augmented state covariance
-  MatriXd P_aug = MatrixXd(n_aug, n_aug);
+  MatriXd P_aug = MatrixXd(n_aug_, n_aug_);
 
   //create sigma point matrix
-  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
 
   // update first column to mean x_ values
   x_aug.head(5) = x_;
@@ -146,13 +146,13 @@ void UKF::GenerateSigmaPoints(double delta_t) {
 
   //create augmented sigma points
   Xsig_aug.col(0) = x_aug;
-  for (int i = 0; i < n_aug; i++){
-    Xsig_aug.col(i+1) = x_aug + sqrt(lambda+n_aug) * L.col(i);
-    Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda+n_aug) * L.col(i);
+  for (int i = 0; i < n_aug_; i++){
+    Xsig_aug.col(i+1) = x_aug_ + sqrt(lambda_+n_aug_) * L.col(i);
+    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
   }
 
   //Predict sigma points
-  for (int i = 0; i < 2*n_aug+1; i++){
+  for (int i = 0; i < 2*n_aug_+1; i++){
     //improve readbility by extracting values
     double p_x = Xsig_aug(0,i);
     double p_y = Xsig_aug(1,i);
@@ -203,6 +203,36 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  //generate sigma points
+  this.GenerateSigmaPoints(delta_t);
+
+  // set weights
+  double weight_0 = lambda_/(lambda_+n_aug_);
+  weights_(0) = weight_0;
+  for (int i=1; i<2*n_aug_+1; i++) {  //2n+1 weights
+    double weight = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight;
+  }
+
+  //predicted state mean
+  x_.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+    x_ = x_+ weights_(i) * Xsig_pred_.col(i);
+  }
+
+  //predicted state covariance matrix
+  P_.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+
+    // state difference
+    VectorXd x_diff = Xsig_pred.col(i) - x_;
+    //angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    P_ = P_ + weights_(i) * x_diff * x_diff.transpose() ;
+  }
 }
 
 /**
@@ -233,4 +263,96 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+
+  // update prediction to measurement space
+
+
+  int n_z = 3;
+  
+  //create matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug_ + 1);
+  //transform sigma points into measurement space
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+
+    // extract values for better readibility
+    double p_x = Xsig_pred_(0,i);
+    double p_y = Xsig_pred_(1,i);
+    double v  = Xsig_pred_(2,i);
+    double yaw = Xsig_pred_(3,i);
+
+    double v1 = cos(yaw)*v;
+    double v2 = sin(yaw)*v;
+
+    // measurement model
+    Zsig(0,i) = sqrt(p_x*p_x + p_y*p_y);                        //r
+    Zsig(1,i) = atan2(p_y,p_x);                                 //phi
+    Zsig(2,i) = (p_x*v1 + p_y*v2 ) / sqrt(p_x*p_x + p_y*p_y);   //r_dot
+  }
+
+  //mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
+  z_pred.fill(0.0);
+  for (int i=0; i < 2*n_aug_+1; i++) {
+      z_pred = z_pred + weights_(i) * Zsig.col(i);
+  }
+
+  //measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z,n_z);
+  S.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+    //residual
+    VectorXd z_diff = Zsig.col(i) - z_pred;
+
+    //angle normalization
+    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+    S = S + weights_(i) * z_diff * z_diff.transpose();
+  }
+
+  //add measurement noise covariance matrix
+  MatrixXd R = MatrixXd(n_z,n_z);
+  R <<    std_radr_*std_radr_, 0, 0,
+          0, std_radphi_*std_radphi_, 0,
+          0, 0,std_radrd_*std_radrd_;
+  S = S + R;
+
+   //create matrix for cross correlation Tc
+  MatrixXd Tc = MatrixXd(n_x, n_z);
+  //calculate cross correlation matrix
+  Tc.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+
+    //residual
+    VectorXd z_diff = Zsig.col(i) - z_pred;
+    //angle normalization
+    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+    // state difference
+    VectorXd x_diff = Xsig_pred.col(i) - x_;
+    //angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
+  }
+
+  //Kalman gain K;
+  MatrixXd K = Tc * S.inverse();
+
+  //residual
+  VectorXd z = VectorXd(3);
+  z << meas_package.raw_measurements_[0],
+       meas_package.raw_measurements_[1],
+       meas_package.raw_measurements_[2];
+  VectorXd z_diff = z - z_pred;
+
+  //angle normalization
+  while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+  while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+  //update state mean and covariance matrix
+  x_ = x_ + K * z_diff;
+  P_ = P_ - K*S*K.transpose();
 }
